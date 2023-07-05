893105617bed4344e011d7d0c77031390b9dd17c
FarrahYasin <farah.y.yassin@gmail.com>
2023-07-05 10:33:59 +0000

    test the test

--- __test__/movies.test.js
+++ __test__/movies.test.js
@@ -15,3 +15,49 @@ afterAll(async () =>{
 })
 
 
+describe('moveis test' , () =>{
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZhcmFoIiwicGFzc3dvcmQiOiIkMmIkMDUkYUQ4LjlDaU5jMC54ZS5Kdk4vL3E5T05DV0pHOHF4aEhxUmNoWFQ1ZjJneldrOHJyUzY4S0siLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2ODg1NDYwMDZ9.qSWmUhhI0ueZ0NCzfcfV_kgrS8FIk0M3lmlahm-vioA'
+
+    it('get all movies' ,async () =>{
+
+         const res = await muckReq.get('/movies').set(`Authorization`,`Basic ${token}`)
+         expect(res.body.message).toBe('Welcome to Movies page ');
+
+    })
+    it('get one movies' ,async () =>{
+
+        const res = await muckReq.get('/movies/:id').set(`Authorization`,`Basic ${token}`)
+        expect(res.body.message).toBe('Welcome to Movies page');
+
+   })
+
+    it('post movie' ,async () =>{
+      
+         const res = await muckReq.post('/movies').set(`Authorization`,`Basic ${token}`).send({
+            name: 'life',
+              type : 'Drama',
+              duration:12,
+              url:'bhdbfhbjhdbkakfka',
+              release_date:2020
+
+         })
+         expect(res.body.message).toBe('Movies created successfully');
+    })
+
+   it('update movies' ,async () =>{
+    const res = await muckReq.put('/movies/:id').set(`Authorization`,`Basic ${token}`).send({
+              name: 'life2',
+              type : 'Drama2',
+              duration:121212,
+              url:'bhdbfdhbjhdbkakfka',
+              release_date:2023
+    })
+    expect(res.body.message).toBe('Movies updated successfully');
+})
+
+it('delete tvShow' ,async () =>{
+    const res = await muckReq.delete('/movies/:id').set(`Authorization`,`Basic ${token}`)
+    expect(res.body.message).toBe('Movies deleted successfully');
+})
+    
+})
--- __test__/tvshows.test.js
+++ __test__/tvshows.test.js
@@ -15,3 +15,43 @@ afterAll(async () =>{
 })
 
 
+describe('tvShows test' , () =>{
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZhcmFoMTEiLCJwYXNzd29yZCI6IiQyYiQwNSR4WldpZnBrZlpQcktKTGZiSFdEM3N1dGtqZ2dDM0lTdFYwZmVkOHlqVGJDeHFyT2VscVdNNiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4ODU1MTUyNX0.MmTrLXU5DNOjm_PnBSIqKaptPYlIRGNdS2ZhLAUvlNE'
+
+
+    it('get all tvShows' ,async () =>{
+
+         const res = await muckReq.get('/tvShows').set(`Authorization`,`Basic ${token}`)
+         expect(res.body.message).toBe('Welcome to tvShows page ');
+
+    })
+    it('get one tvShows' ,async () =>{
+
+        const res = await muckReq.get('/tvShows/:id').set(`Authorization`,`Basic ${token}`)
+        expect(res.body.message).toBe('Welcome to tvShows page');
+
+   })
+    it('post tvShow' ,async () =>{
+      
+         const res = await muckReq.post('/tvShows').set(`Authorization`,`Basic ${token}`).send({
+            name: 'life',
+              type : 'Drama'
+         })
+         expect(res.body.message).toBe('tvShows created successfully');
+    })
+    it('delete tvShow' ,async () =>{
+        const res = await muckReq.delete('/tvShows/:id').set(`Authorization`,`Basic ${token}`)
+        expect(res.body.message).toBe('tvShows deleted successfully');
+   })
+   
+   it('update tvShow' ,async () =>{
+    const res = await muckReq.put('/tvShows/:id').set(`Authorization`,`Basic ${token}`).send({
+       name: 'life1',
+         type : 'Drama1'
+    })
+    expect(res.body.message).toBe('tvShows updated successfully');
+})
+
+
+})
+
